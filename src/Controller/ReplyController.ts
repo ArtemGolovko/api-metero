8e070a29476d0f7fae0ff1168b0428e68689ae5b
Artem Golovko <artem.golovko2008@gmail.com>
2022-05-04 20:07:13 +0300

    Add reply controller

---
+++ src/Controller/ReplyController.ts
@@ -0,0 +1,10 @@
+import Router, { IMiddleware } from "koa-router";
+import AbstractController from "./AbstractController";
+
+export default class ReplyController extends AbstractController {
+    public routes(): IMiddleware<any, {}> {
+        const router = new Router();
+
+        return router.routes();
+    }
+}
--- src/Controller/Router.ts
+++ src/Controller/Router.ts
@@ -1,6 +1,7 @@
 import Router from "koa-router";
 import CommentController from "./CommentController";
 import PostController from "./PostController";
+import ReplyController from "./ReplyController";
 import UserController from "./UserController";
 
 const router = new Router();
@@ -8,9 +9,11 @@ const router = new Router();
 const userController = new UserController();
 const postController = new PostController();
 const commentController = new CommentController();
+const replyController = new ReplyController();
 
 router.use(userController.prefix(), userController.routes());
 router.use(postController.prefix(), postController.routes());
 router.use(commentController.routes());
+router.use(replyController.routes());
 
 export default router;
