cbe05cfe3fdb1b2ec6ab34b4d942e9bbd0cb8159
Artem Golovko <artem.golovko2008@gmail.com>
2022-05-05 16:24:29 +0300

    Fix acam format

--- src/Security/browserSecurity.ts
+++ src/Security/browserSecurity.ts
@@ -3,7 +3,7 @@ import type { Next, Context } from "koa";
 export const CORSandCSP = async (ctx: Context, next: Next) => {
     ctx.set({
         'Access-Control-Allow-Origin': ctx.request.header['origin'] ?? '*',
-        'Access-Control-Allow-Methods': 'GET POST PUT DELETE OPTIONS HEAD',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS, HEAD',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         'Access-Control-Max-Age': '86400',
         'Content-Security-Policy': 'default-src ' + (ctx.request.header['origin'] ?? '*')
